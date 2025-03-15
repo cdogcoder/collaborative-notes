44f390410cdb644e1c26860c9c889cb462aaf11e
cdogcoder <cdogcoder@gmail.com>
2025-03-15 10:16:38 -0700

    Create functionality to save all document changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
 export default function Home() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const storedText = window.localStorage.getItem("myText");
+    storedText !== null ? setText(JSON.parse(storedText)) : "";
+  }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem("myText", JSON.stringify(text));
+  }, [text]);
+
   return (
     <div className="body">
-      <textarea className="document"></textarea>
+      <textarea className="document" onKeyUp={(event) => {setText(text+event.key)}} defaultValue={text}></textarea>
     </div>
   );
 }
