1cfcaed501466e9ddf3af0dc109aea78c98e0006
cdogcoder <cdogcoder@gmail.com>
2025-03-15 11:21:13 -0700

    Rewrite document as a separate component from home

---
+++ src/app/components/document.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
+
+export default function Document() {
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        const storedText = window.localStorage.getItem("myText");
+        storedText !== null ? setText(JSON.parse(storedText)) : "";
+    }, [])
+
+    useEffect(() => {
+        window.localStorage.setItem("myText", JSON.stringify(text));
+    }, [text]);
+
+    return (
+    <textarea className="document" onKeyUp={() => {
+        const textArea = document.querySelector('textarea');
+        if (textArea !== null) setText(textArea.value);
+      }} defaultValue={text}></textarea>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,8 @@
-'use client';
-
-import React, { useState, useEffect } from 'react';
-
+import Document from "./components/document";
 export default function Home() {
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    const storedText = window.localStorage.getItem("myText");
-    storedText !== null ? setText(JSON.parse(storedText)) : "";
-  }, [])
-
-  useEffect(() => {
-    window.localStorage.setItem("myText", JSON.stringify(text));
-  }, [text]);
-
   return (
     <div className="body">
-      <textarea className="document" onKeyUp={() => {
-        const textArea = document.querySelector('textarea');
-        if (textArea !== null) setText(textArea.value);
-      }} defaultValue={text}></textarea>
+      <Document></Document>
     </div>
   );
 }
