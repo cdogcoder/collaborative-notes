2ce0b8f94d87e2403bd6eb1b8c3c8c971b0b68e0
cdogcoder <cdogcoder@gmail.com>
2025-03-15 18:15:15 -0700

    Create title bar
    The title bar will supply the document's title for
    the save button to store in the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import SaveButton from "./components/save-button";
 
 export default function Home() {
   const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
       const storedText = window.localStorage.getItem("myText");
@@ -17,16 +18,25 @@ export default function Home() {
 
   return (
     <div className="body">
+      <input 
+        type="text" 
+        className="title-bar"
+        onKeyUp={() => {
+          const titleBar = document.querySelector('input[type="text"]');
+          if (titleBar !== null) setTitle(titleBar.value);
+        }}
+        defaultValue={title}
+      />
       <textarea 
         className="document" 
         onKeyUp={() => {
-            const textArea = document.querySelector('textarea');
-            if (textArea !== null) setText(textArea.value);
+          const textArea = document.querySelector('textarea');
+          if (textArea !== null) setText(textArea.value);
         }} 
         defaultValue={text}
       >
       </textarea>
-      <SaveButton documentText={text} documentTitle={"hi"}></SaveButton>
+      <SaveButton documentText={text} documentTitle={title}></SaveButton>
     </div>
   );
 }
