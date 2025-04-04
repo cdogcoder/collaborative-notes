832b51e98f03a9bd9de51664803afae211539a8d
cdogcoder <cdogcoder@gmail.com>
2025-04-03 20:47:41 -0700

    Implement document saving feature with associated button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { addDoc, collection, doc } from "firebase/firestore";
 import { db } from "./config/firebase";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [documentTitle, setDocumentTitle] = useState("");
@@ -12,21 +12,37 @@ export default function Home() {
     storedTitle !== null ? setDocumentTitle(JSON.parse(storedTitle)) : "";
     const storedText = window.localStorage.getItem("documentPageText");
     storedText !== null ? setDocumentPageText(JSON.parse(storedText)) : "";
-  }, [])
+  }, []);
   useEffect(() => {
     window.localStorage.setItem("documentTitle", JSON.stringify(documentTitle));
-    window.localStorage.setItem("documentPageText", JSON.stringify(documentPageText));
+    window.localStorage.setItem(
+      "documentPageText",
+      JSON.stringify(documentPageText)
+    );
   }, [documentPageText]);
 
+  const saveDocument = async () => {
+    const collectionRef = collection(db, "documents");
+
+    const docRef = await addDoc(collectionRef, {
+      documentTitle: documentTitle,
+      documentText: documentPageText,
+    });
+  };
 
   return (
     <div className="body">
-      <input
-        className="document-title"
-        type="text"
-        onKeyUp={(event) => setDocumentTitle(event.target.value)}
-        defaultValue={documentTitle}
-      />
+      <div className="navigation-bar">
+        <input
+          className="document-title"
+          type="text"
+          onKeyUp={(event) => setDocumentTitle(event.target.value)}
+          defaultValue={documentTitle}
+        />
+        <button className="save-document-button" onClick={() => saveDocument()}>
+          Save
+        </button>
+      </div>
       <textarea
         className="document-page"
         onKeyUp={(event) => setDocumentPageText(event.target.value)}
