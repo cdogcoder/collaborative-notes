dd3d9be4ea94cde87f0415c14b24423cbcbe5d22
cdogcoder <cdogcoder@gmail.com>
2025-04-03 20:27:02 -0700

    Implement local storage to save document title and page text

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,36 @@
 "use client";
 import { addDoc, collection, doc } from "firebase/firestore";
 import { db } from "./config/firebase";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 export default function Home() {
   const [documentTitle, setDocumentTitle] = useState("");
   const [documentPageText, setDocumentPageText] = useState("");
+
+  useEffect(() => {
+    const storedTitle = window.localStorage.getItem("documentTitle");
+    storedTitle !== null ? setDocumentTitle(JSON.parse(storedTitle)) : "";
+    const storedText = window.localStorage.getItem("documentPageText");
+    storedText !== null ? setDocumentPageText(JSON.parse(storedText)) : "";
+  }, [])
+  useEffect(() => {
+    window.localStorage.setItem("documentTitle", JSON.stringify(documentTitle));
+    window.localStorage.setItem("documentPageText", JSON.stringify(documentPageText));
+  }, [documentPageText]);
+
 
   return (
     <div className="body">
       <input
         className="document-title"
         type="text"
-        onKeyUp={(event) => console.log(event.target.value)}
+        onKeyUp={(event) => setDocumentTitle(event.target.value)}
+        defaultValue={documentTitle}
       />
       <textarea
         className="document-page"
-        onKeyUp={(event) => console.log(event.target.value)}
+        onKeyUp={(event) => setDocumentPageText(event.target.value)}
+        defaultValue={documentPageText}
       ></textarea>
     </div>
   );
