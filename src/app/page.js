91c7b0ffef9e9ec5ceca444067e8ccf916170bd5
cdogcoder <cdogcoder@gmail.com>
2025-04-03 18:44:48 -0700

    Initialize and test Firestore connection

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+'use client'
+import { addDoc, collection, doc } from "firebase/firestore";
+import { db } from "./config/firebase";
+
 export default function Home() {
+  const saveDocument = async () => {
+    const collectionRef = collection(db, 'documents');
+
+    const docRef = await addDoc(collectionRef, {
+      title: 'hi',
+      content: 'hi2'
+    })
+  }
+
   return (
     <div>
-      Hello, World!
+      <button onClick={()=>saveDocument()}>Click me</button>
     </div>
   );
 }
