aab0b17289e2dc72f78829cbcefd9b030568fdb6
cdogcoder <cdogcoder@gmail.com>
2025-04-03 20:18:01 -0700

    Create basic HTML structure of document page

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,23 @@
-'use client'
+"use client";
 import { addDoc, collection, doc } from "firebase/firestore";
 import { db } from "./config/firebase";
+import { useState } from "react";
 
 export default function Home() {
-  const saveDocument = async () => {
-    const collectionRef = collection(db, 'documents');
-
-    const docRef = await addDoc(collectionRef, {
-      title: 'hi',
-      content: 'hi2'
-    })
-  }
+  const [documentTitle, setDocumentTitle] = useState("");
+  const [documentPageText, setDocumentPageText] = useState("");
 
   return (
-    <div>
-      <button onClick={()=>saveDocument()}>Click me</button>
+    <div className="body">
+      <input
+        className="document-title"
+        type="text"
+        onKeyUp={(event) => console.log(event.target.value)}
+      />
+      <textarea
+        className="document-page"
+        onKeyUp={(event) => console.log(event.target.value)}
+      ></textarea>
     </div>
   );
 }
