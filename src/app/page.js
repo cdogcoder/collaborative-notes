ca0d1553df4716009398fc5bf3fa4c17d612898d
cdogcoder <cdogcoder@gmail.com>
2025-04-04 09:47:15 -0700

    Create home page to contain documents in one place

--- src/app/page.js
+++ src/app/page.js
@@ -4,73 +4,27 @@ import { db } from "./config/firebase";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [documentTitle, setDocumentTitle] = useState("");
-  const [documentPageText, setDocumentPageText] = useState("");
+  const [documents, setDocuments] = useState([]);
 
-  useEffect(() => {
-    const storedTitle = window.localStorage.getItem("documentTitle");
-    storedTitle !== null ? setDocumentTitle(JSON.parse(storedTitle)) : "";
-    const storedText = window.localStorage.getItem("documentPageText");
-    storedText !== null ? setDocumentPageText(JSON.parse(storedText)) : "";
-  }, []);
-  useEffect(() => {
-    window.localStorage.setItem("documentTitle", JSON.stringify(documentTitle));
-    window.localStorage.setItem(
-      "documentPageText",
-      JSON.stringify(documentPageText)
-    );
-  }, [documentPageText]);
-
-  const saveDocument = async () => {
+  const displayDocuments = async () => {
     const collectionRef = collection(db, "documents");
+    const documentsSnapshot = await getDocs(collectionRef);
+    const documentsSaved = documentsSnapshot.docs.map((doc) => ({
+      ...doc.data(), id: doc.id
+    }))
+    setDocuments(documentsSaved);
+  }
 
-    // const documentsSnapshot = await getDocs(collectionRef);
-    // const documentsSaved = documentsSnapshot.docs.map((doc) => ({
-    //   ...doc.data(), id: doc.id
-    // }))
+  useEffect(() => {
+    displayDocuments()
+  }, [])
 
-    // const currDocument = documentsSaved.find((document) => {
-    //   return document.documentTitle == documentTitle && document.documentText == documentPageText
-    // })
-
-    // if (currDocument) {
-    //   const docRef = doc(db, 'documents', currDocument.id);
-
-    //   await updateDoc(docRef, {
-    //     documentTitle: documentTitle,
-    //     documentText: documentPageText,
-    //   })
-    // } else {
-    //   const docRef = await addDoc(collectionRef, {
-    //     documentTitle: documentTitle,
-    //     documentText: documentPageText,
-    //   });
-    // }
-    const docRef = await addDoc(collectionRef, {
-      documentTitle: documentTitle,
-      documentText: documentPageText,
-    });
-
-  };
-
+  console.log(documents)
   return (
-    <div className="body">
-      <div className="navigation-bar">
-        <input
-          className="document-title"
-          type="text"
-          onKeyUp={(event) => setDocumentTitle(event.target.value)}
-          defaultValue={documentTitle}
-        />
-        <button className="save-document-button" onClick={() => saveDocument()}>
-          Save
-        </button>
-      </div>
-      <textarea
-        className="document-page"
-        onKeyUp={(event) => setDocumentPageText(event.target.value)}
-        defaultValue={documentPageText}
-      ></textarea>
+    <div className="documents-container">
+      {documents.map((document) => {
+        return <button key={document.id}>{document.title} {document.id}</button>
+      })}
     </div>
   );
 }
