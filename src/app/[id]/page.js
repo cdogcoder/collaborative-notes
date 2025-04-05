c08988b824fd5f08be048754350ceb2da3150dda
cdogcoder <cdogcoder@gmail.com>
2025-04-04 21:54:45 -0700

    Implement document updating feature
    This was done by creating a dynamic route for document
    pages.

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,76 +1,65 @@
 "use client";
-import { addDoc, collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 
 export default function DocumentPage() {
-    const [documentTitle, setDocumentTitle] = useState("");
-    const [documentPageText, setDocumentPageText] = useState("");
+  const params = useParams();
+  const id = params.id;
+  const [documentTitle, setDocumentTitle] = useState("");
+  const [documentPageText, setDocumentPageText] = useState("");
 
-    useEffect(() => {
-        const storedTitle = window.localStorage.getItem("documentTitle");
-        storedTitle !== null ? setDocumentTitle(JSON.parse(storedTitle)) : "";
-        const storedText = window.localStorage.getItem("documentPageText");
-        storedText !== null ? setDocumentPageText(JSON.parse(storedText)) : "";
-    }, []);
-    useEffect(() => {
-        window.localStorage.setItem("documentTitle", JSON.stringify(documentTitle));
-        window.localStorage.setItem(
-        "documentPageText",
-        JSON.stringify(documentPageText)
-        );
-    }, [documentPageText]);
-
-    const saveDocument = async () => {
-        const collectionRef = collection(db, "documents");
-
-        // const documentsSnapshot = await getDocs(collectionRef);
-        // const documentsSaved = documentsSnapshot.docs.map((doc) => ({
-        //   ...doc.data(), id: doc.id
-        // }))
-
-        // const currDocument = documentsSaved.find((document) => {
-        //   return document.documentTitle == documentTitle && document.documentText == documentPageText
-        // })
-
-        // if (currDocument) {
-        //   const docRef = doc(db, 'documents', currDocument.id);
-
-        //   await updateDoc(docRef, {
-        //     documentTitle: documentTitle,
-        //     documentText: documentPageText,
-        //   })
-        // } else {
-        //   const docRef = await addDoc(collectionRef, {
-        //     documentTitle: documentTitle,
-        //     documentText: documentPageText,
-        //   });
-        // }
-        const docRef = await addDoc(collectionRef, {
-            documentTitle: documentTitle,
-            documentText: documentPageText,
-        });
-
-    };
-
-    return (
-        <div className="body">
-        <div className="navigation-bar">
-            <input
-            className="document-title"
-            type="text"
-            onKeyUp={(event) => setDocumentTitle(event.target.value)}
-            defaultValue={documentTitle}
-            />
-            <button className="save-document-button" onClick={() => saveDocument()}>
-            Save
-            </button>
-        </div>
-        <textarea
-            className="document-page"
-            onKeyUp={(event) => setDocumentPageText(event.target.value)}
-            defaultValue={documentPageText}
-        ></textarea>
-        </div>
+  useEffect(() => {
+    const storedTitle = window.localStorage.getItem("documentTitle");
+    storedTitle !== null ? setDocumentTitle(JSON.parse(storedTitle)) : "";
+    const storedText = window.localStorage.getItem("documentPageText");
+    storedText !== null ? setDocumentPageText(JSON.parse(storedText)) : "";
+  }, []);
+  useEffect(() => {
+    window.localStorage.setItem("documentTitle", JSON.stringify(documentTitle));
+    window.localStorage.setItem(
+      "documentPageText",
+      JSON.stringify(documentPageText)
     );
+  }, [documentPageText]);
+
+  const saveDocument = async () => {
+    const collectionRef = collection(db, "documents");
+
+    const docRef = doc(db, "documents", id);
+
+    if (docRef) {
+      await updateDoc(docRef, {
+        documentTitle: documentTitle,
+        documentText: documentPageText,
+      });
+    } else {
+      await addDoc(collectionRef, {
+        documentTitle: documentTitle,
+        documentText: documentPageText,
+      });
+    }
+  };
+
+  return (
+    <div className="body">
+      <div className="navigation-bar">
+        <input
+          className="document-title"
+          type="text"
+          onKeyUp={(event) => setDocumentTitle(event.target.value)}
+          defaultValue={documentTitle}
+        />
+        <button className="save-document-button" onClick={() => saveDocument()}>
+          Save
+        </button>
+      </div>
+      <textarea
+        className="document-page"
+        onKeyUp={(event) => setDocumentPageText(event.target.value)}
+        defaultValue={documentPageText}
+      ></textarea>
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,34 @@
 "use client";
-import { addDoc, collection, doc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [documents, setDocuments] = useState([]);
 
   const displayDocuments = async () => {
     const collectionRef = collection(db, "documents");
-
-    const docRef = await addDoc(collectionRef, {
-      documentTitle: documentTitle,
-      documentText: documentPageText,
-    });
+    const documentsSnapshot = await getDocs(collectionRef);
+    const documentsSaved = documentsSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setDocuments(documentsSaved);
   };
+
+  useEffect(() => {
+    displayDocuments();
+  }, []);
 
   return (
     <div className="documents-container">
       {documents.map((document) => {
-        return <button key={document.id}>{document.title} {document.id}</button>
+        return (
+          <Link href={`/${document.id}`} key={document.id}>
+            {document.title} {document.id}
+          </Link>
+        );
       })}
     </div>
   );
