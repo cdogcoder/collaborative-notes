82ef5df3f558a6e3a8be57afff19a0a78b810295
cdogcoder <cdogcoder@gmail.com>
2025-04-05 11:03:03 -0700

    Implement adding new document feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 "use client";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "./config/firebase";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const [documents, setDocuments] = useState([]);
+  const collectionRef = collection(db, "documents");
 
   const displayDocuments = async () => {
-    const collectionRef = collection(db, "documents");
     const documentsSnapshot = await getDocs(collectionRef);
     const documentsSaved = documentsSnapshot.docs.map((doc) => ({
       ...doc.data(),
@@ -21,9 +22,17 @@ export default function Home() {
     displayDocuments();
   }, []);
 
+  const addNewDocument = async () => {
+    const docRef = await addDoc(collectionRef, {
+      documentTitle: "",
+      documentText: "",
+    });
+    redirect(`/${docRef.id}`);
+  }
+
   return (
     <>
-      <button className="add-new-document-button">Add New Document</button>
+      <button className="add-new-document-button" onClick={() => addNewDocument()}>Add New Document</button>
       <div className="documents-container">
         {documents.map((document) => {
           return (
