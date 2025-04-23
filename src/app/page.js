43b68255a1c687a07660b3218e611daa699db1df
cdogcoder <cdogcoder@gmail.com>
2025-04-22 18:00:50 -0700

    Add dynamic sign up status message

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import { auth } from "./config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 export default function Home() {
   const router = useRouter();
+  const [userSignedUp, setUserSignedUp] = useState(false);
   
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUserSignedUp(true)
+    }
+  })
+
   return (
     <div className="body homepage">
       <nav className="header-nav">
@@ -20,6 +27,7 @@ export default function Home() {
         <h1 className="hero-text">Collaborative Notes</h1>
         <p className="description">Google Docs, with AI, only worse. Your one-stop-shop for literally nothing. {":)"}</p>
       </div>
+      <div className="sign-up-status-message" hidden={!userSignedUp}>You've signed up! Please login with the credentials you used to create your account.</div>
     </div>
   );
 }
